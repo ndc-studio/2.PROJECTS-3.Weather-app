995a04d49e0a077cc81547ce5c13e5c52feb3eec
pc-portable <martin.stephan9218@gmail.com>
2025-04-03 16:46:14 +0200

    Try to resolve APIKEY error

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  define: {
-    'process.env.VITE_WEATHER_API_KEY': JSON.stringify(process.env.VITE_WEATHER_API_KEY),
-    'process.env.VITE_PICTURES_API_KEY': JSON.stringify(process.env.VITE_PICTURES_API_KEY),
-  },
-});
