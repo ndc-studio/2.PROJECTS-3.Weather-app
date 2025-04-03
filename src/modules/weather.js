d299499ed812105a2c9ae158939190a1fb884834
pc-portable <martin.stephan9218@gmail.com>
2025-04-03 13:21:17 +0200

    Try to fix errors

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -1,8 +1,7 @@
 import { getPicture } from "./city_picture.js";
 import { getDate } from "./timestamp.js";
 
-const apiKey = '33540e55ad4298c6759091a17506103e'; // Clé api
-
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY; // Clé api
 /**
  * 
  * @description -- Recupère l'id et la class de l'option selectionnée
