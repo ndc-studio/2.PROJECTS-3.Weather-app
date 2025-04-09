08ec1d0f826aba695a405b38925b6971b7e502d7
NDC Studio <ndc.dev.code@gmail.com>
2025-04-09 15:45:18 +0200

    Permanently set the API key so the website works correctly.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { getWeatherApiKey } from "./modules/weather.js";
 
 getCountries();
 getMode();
+
 /*
 let pictApiKey = import.meta.env.VITE_PICTURES_API_KEY;
 let weatherApiKey = import.meta.env.VITE_WEATHER_API_KEY;
@@ -12,7 +13,6 @@ let weatherApiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
 let pictApiKey = "cRYK1UNWamzF3f3MrVKYZ3DXDwA1HyNE8ggWYjpY6ms"
 let weatherApiKey = "33540e55ad4298c6759091a17506103e"
-
 
 getWeatherApiKey(weatherApiKey);
 getPictApiKey(pictApiKey);
