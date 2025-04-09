644f8820251b586d22daf667ac476ec28c755e46
NDC Studio <ndc.dev.code@gmail.com>
2025-04-09 10:08:10 +0200

    Try again to hide API KEYS in secrets and variable

--- src/main.js
+++ src/main.js
@@ -2,20 +2,18 @@ import { getPictApiKey } from "./modules/city_picture.js";
 import { getCountries, getCountryApiKey } from "./modules/countries.js";
 import { getMode } from "./modules/mode.js";
 import { getWeatherApiKey } from "./modules/weather.js";
-import {VITE_PICTURES_API_KEY} from "../env.js";
-import {VITE_WEATHER_API_KEY} from "../env.js";
 
 getCountries();
 getMode();
 
-let pictApiKey = VITE_PICTURES_API_KEY;
+let pictApiKey = import.meta.env.VITE_PICTURES_API_KEY;
 
-let weatherApiKey = VITE_WEATHER_API_KEY;
+let weatherApiKey = import.meta.env.VITE_WEATHER_API_KEY;
+/*
+let pictApiKey = "cRYK1UNWamzF3f3MrVKYZ3DXDwA1HyNE8ggWYjpY6ms"
 
-// let pictApiKey = "cRYK1UNWamzF3f3MrVKYZ3DXDwA1HyNE8ggWYjpY6ms"
-
-// let weatherApiKey = "33540e55ad4298c6759091a17506103e"
-
+let weatherApiKey = "33540e55ad4298c6759091a17506103e"
+*/
 getWeatherApiKey(weatherApiKey);
 getPictApiKey(pictApiKey);
 getCountryApiKey(weatherApiKey);
